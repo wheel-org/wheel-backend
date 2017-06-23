0912371459c28b4863212efc53d8b8937880bec7
Michael Kim <micklepickle98@gmail.com>
2017-06-23 12:25:24 -0700

    Leave rooms

--- routes/rooms/index.js
+++ routes/rooms/index.js
@@ -3,7 +3,8 @@ var router = require("express").Router();
 router.use('/create', require('./create'));
 router.use('/join', require('./join'));
 router.use('/get', require('./get'));
-//router.use('/leave', require('./leave'));
+router.use('/leave', require('./leave'));
 //router.use('/delete', require('./delete'));
+//router.use('/update', require('./update'));
 
 module.exports = router;
---
+++ routes/rooms/leave.js
@@ -0,0 +1,77 @@
+var router = require("express").Router();
+var firebase = require("../../firebase").database();
+
+router.post('/', function(req, res, next) {
+    console.log("Leaving room");
+    console.log(req.body);
+    var query = {
+        id: req.body.id
+    };
+
+    firebase.ref("rooms/" + query.id).once("value", function(snap) {
+        var data = snap.val();
+        console.log(data);
+
+        // Room doesn't exist
+        if(data === undefined || data === null) {
+            return next({
+                code: 6,
+                msg: 'Room not found'
+            });
+        }
+
+        // User not in room
+        if(data.usernames.indexOf(req.user.username) === -1) {
+            return next({
+                code: 4,
+                msg: 'User is not in room'
+            });
+        };
+
+        // Remove room from user
+        firebase.ref('users/' + req.user.username + '/rooms/').once('value', function(snap) {
+            var rooms = snap.val();
+
+            delete rooms[query.id];
+
+            firebase.ref('users/' + req.user.username + '/rooms/').set(rooms);
+        }, function(error) {
+            return next({
+                code: 0,
+                msg: error
+            });
+        });
+
+        // Remove user from room
+        var index = data.usernames.indexOf(req.user.username);
+        if(index > -1) {
+            data.usernames.splice(index, 1);
+        }
+        if(data.usernames.length <= 0) {
+            // Delete room
+        }
+
+        // Remove user's transactions from room
+        for(var id in data.transactions) {
+            if(data.transactions.hasOwnProperty(id)) {
+                if(data.transactions[id].username === req.user.username) {
+                    delete data.transactions[id];
+                }
+            }
+        }
+
+        firebase.ref('rooms/' + query.id).set(data);
+
+        res.send({
+            success: true,
+            data: {}
+        });
+    }, function (error) {
+        next({
+            code: 0,
+            msg: error
+        });
+    });
+});
+
+module.exports = router;
